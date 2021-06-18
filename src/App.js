fd07dd7842815346446dbbcec5ac68e6a8564bf2
abhaywwe7 <abhaywwe7@gmail.com>
2021-06-18 23:45:46 +0530

    added css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Board from '../src/components/Board';
 import './styles/root.css';
 const App = () => {
   return (
-    <>
+    <div className="app">
       <h1>Tic Tac Toe</h1>
       <Board />
-    </>
+    </div>
   );
 };
 
