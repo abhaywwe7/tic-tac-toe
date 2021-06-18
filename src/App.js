36e23a76c18ce09288ba631e10215dbd8394c1e6
abhaywwe7 <abhaywwe7@gmail.com>
2021-06-18 23:08:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from '../src/components/Board';
+import './styles/root.css';
+const App = () => {
+  return (
+    <>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </>
+  );
+};
+
+export default App;
